fc89330889dab736cbc7bb9e7a9ae399c42c6bfe
Tifan <tifan.avianto@shopee.com>
2023-12-18 16:38:43 +0700

    feat: add pagination and sort functionality

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import React from "react"
+import type { GetServerSideProps, InferGetServerSidePropsType } from "next"
 import Image from "next/image"
 import { useRouter } from "next/router"
 import {
@@ -27,39 +28,47 @@ import { RootLayout } from "@/components/layouts/root"
 import { Shell } from "@/components/shell"
 import { TrendingPosts } from "@/components/trending-posts"
 
-// export const getServerSideProps: GetServerSideProps<{
-//   posts: Post[]
-// }> = async (context) => {
-//   const page = (context.query?.page as string) ?? "1"
-//   const limit = String(Number(page) * 10)
+export const getServerSideProps: GetServerSideProps<{
+  page: string
+  sort: "asc" | "desc"
+}> = async (context) => {
+  const page = (context.query?.page as string) ?? "1"
+  const sort = (context.query?.sort as "asc" | "desc") ?? "desc"
 
-//   const url = new URL("/posts", process.env.NEXT_PUBLIC_DB_URL)
-//   url.searchParams.append("_page", page)
-//   url.searchParams.append("_limit", limit)
-
-//   const res = await fetch(url)
-
-//   const posts = await res.json()
-
-//   return {
-//     props: {
-//       posts,
-//     },
-//   }
-// }
+  return {
+    props: {
+      page,
+      sort,
+    },
+  }
+}
 
 const PAGE_SIZE = 10
 
-export default function HomePage() {
+export default function HomePage({
+  page,
+  sort,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
   const router = useRouter()
 
-  const { data, isLoading, size, setSize } = useSWRInfinite(
-    (idx) => `/posts?_page=${idx + 1}`,
+  const [sortOrder, setSortOrder] = React.useState<"asc" | "desc">(sort)
+
+  const { data, isLoading, size, setSize, mutate } = useSWRInfinite(
+    (idx) => {
+      const params = new URLSearchParams()
+      params.set("_sort", "id")
+      params.set("_order", sortOrder)
+      params.set("_page", String(idx + 1))
+      params.set("_limit", String(PAGE_SIZE))
+
+      return `/posts?${params.toString()}}`
+    },
     fetcher<Post[]>,
     {
-      initialSize: 1,
-      persistSize: true,
+      initialSize: Number(page),
       revalidateOnFocus: false,
+      persistSize: true,
+      parallel: true,
     },
   )
 
@@ -70,31 +79,64 @@ export default function HomePage() {
   const isReachingEnd =
     isEmpty || (data && (data[data.length - 1]?.length ?? 0) < PAGE_SIZE)
 
+  React.useEffect(() => {
+    if (size === 1) return
+
+    router.replace(
+      {
+        query: {
+          ...router.query,
+          page: size,
+        },
+      },
+      undefined,
+      { shallow: true, scroll: false },
+    )
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [size])
+
+  React.useEffect(() => {
+    if (router.query.sort === undefined) return
+    if (router.query.sort === sortOrder) return
+
+    setSortOrder(router.query.sort as "asc" | "desc")
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [router.query.sort])
+
+  console.log(sortOrder)
+
   return (
     <Shell className="max-w-screen-xl grid-cols-12 items-start gap-10 px-14">
       <section className="col-span-8">
         <div className="mb-4 space-x-4">
           <DropdownMenu>
             <DropdownMenuTrigger asChild>
-              <Button
-                variant="outline"
-                size="sm"
-                className="h-8 data-[state=open]:bg-accent"
-              >
-                <span>Foo</span>
-                {/* {column.getIsSorted() === "desc" ? (
-                <ArrowDownIcon className="ml-2 h-4 w-4" aria-hidden="true" />
-              ) : column.getIsSorted() === "asc" ? (
-                <ArrowUpIcon className="ml-2 h-4 w-4" aria-hidden="true" />
-              ) : ( */}
-                <CaretSortIcon className="ml-2 h-4 w-4" aria-hidden="true" />
-                {/* )} */}
+              <Button variant="outline" className="data-[state=open]:bg-accent">
+                <span>Date</span>
+                {sortOrder === "desc" ? (
+                  <ArrowDownIcon className="ml-2 h-4 w-4" aria-hidden="true" />
+                ) : (
+                  <ArrowUpIcon className="ml-2 h-4 w-4" aria-hidden="true" />
+                )}
               </Button>
             </DropdownMenuTrigger>
             <DropdownMenuContent align="start">
               <DropdownMenuItem
                 aria-label="Sort ascending"
-                // onClick={() => column.toggleSorting(false)}
+                onClick={() => {
+                  router.replace(
+                    {
+                      query: {
+                        ...router.query,
+                        sort: "asc",
+                      },
+                    },
+                    undefined,
+                    { shallow: true, scroll: false },
+                  )
+                }}
               >
                 <ArrowUpIcon
                   className="mr-2 h-3.5 w-3.5 text-muted-foreground/70"
@@ -104,7 +146,18 @@ export default function HomePage() {
               </DropdownMenuItem>
               <DropdownMenuItem
                 aria-label="Sort descending"
-                // onClick={() => column.toggleSorting(true)}
+                onClick={() => {
+                  router.replace(
+                    {
+                      query: {
+                        ...router.query,
+                        sort: "desc",
+                      },
+                    },
+                    undefined,
+                    { shallow: true, scroll: false },
+                  )
+                }}
               >
                 <ArrowDownIcon
                   className="mr-2 h-3.5 w-3.5 text-muted-foreground/70"
@@ -118,48 +171,59 @@ export default function HomePage() {
         </div>
 
         <div className="space-y-4">
-          {posts.map((post) => (
-            <article
-              key={post.id}
-              className="flex flex-col gap-4 rounded-lg border bg-card p-6 text-card-foreground shadow-sm"
-            >
-              <div className="flex gap-4">
-                <div className="w-full">
-                  <h2 className="mb-2 text-2xl font-bold tracking-tight">
-                    {post.id} - {post.title}
-                  </h2>
-                  <p className="line-clamp-3">{post.content}</p>
+          {posts.map((post, i) => (
+            <>
+              {i % PAGE_SIZE === 0 && i !== 0 && (
+                <div className="relative border-b border-border/80">
+                  <div className="absolute left-1/2 top-[-1rem] -translate-x-1/2 bg-background p-2 text-xs uppercase leading-[1rem] tracking-wider text-muted-foreground">
+                    Page {i / PAGE_SIZE + 1}
+                  </div>
                 </div>
-                <div className="min-w-[180px]">
-                  <AspectRatio ratio={16 / 10}>
-                    <Image
-                      className="absolute h-[112.5px] w-[180px] rounded-lg object-cover"
-                      width="1600"
-                      height="1000"
-                      src={post.image}
-                      alt="thumbnail"
-                    />
-                  </AspectRatio>
+              )}
+              <article
+                key={post.id}
+                className="flex flex-col gap-4 rounded-lg border bg-card p-6 text-card-foreground shadow-sm"
+              >
+                <div className="flex gap-4">
+                  <div className="w-full">
+                    <h2 className="mb-2 text-2xl font-bold tracking-tight">
+                      {post.id} - {post.title}
+                    </h2>
+                    <p className="line-clamp-3">{post.content}</p>
+                  </div>
+                  <div className="min-w-[180px]">
+                    <AspectRatio ratio={16 / 10}>
+                      <Image
+                        className="absolute h-[112.5px] w-[180px] rounded-lg object-cover"
+                        width="1600"
+                        height="1000"
+                        src={post.image}
+                        alt="thumbnail"
+                      />
+                    </AspectRatio>
+                  </div>
                 </div>
-              </div>
-              <div className="flex justify-between gap-4 text-muted-foreground">
-                <div className="inline-flex items-center gap-2">
-                  <time dateTime={post.updatedAt}>
-                    {formatDate(post.updatedAt)}
-                  </time>
-                  <span className="text-xs text-muted-foreground">&bull;</span>
-                  <span>{post.likes + " likes"}</span>
+                <div className="flex justify-between gap-4 text-muted-foreground">
+                  <div className="inline-flex items-center gap-2">
+                    <time dateTime={post.updatedAt}>
+                      {formatDate(post.updatedAt)}
+                    </time>
+                    <span className="text-xs text-muted-foreground">
+                      &bull;
+                    </span>
+                    <span>{post.likes + " likes"}</span>
+                  </div>
+                  <div className="inline-flex gap-2">
+                    <Badge variant="secondary">
+                      {toSentenceCase(post.category)}
+                    </Badge>
+                    {post.isPremium && (
+                      <Badge className="bg-yellow-600">Premium</Badge>
+                    )}
+                  </div>
                 </div>
-                <div className="inline-flex gap-2">
-                  <Badge variant="secondary">
-                    {toSentenceCase(post.category)}
-                  </Badge>
-                  {post.isPremium && (
-                    <Badge className="bg-yellow-600">Premium</Badge>
-                  )}
-                </div>
-              </div>
-            </article>
+              </article>
+            </>
           ))}
           {(isLoading || isLoadingMore) &&
             Array.from({ length: PAGE_SIZE }).map((_, i) => (
