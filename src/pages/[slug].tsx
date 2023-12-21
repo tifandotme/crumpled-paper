65cbc7093cc2233366f5bd9d3dddc88d775a7342
Tifan <tifan.avianto@shopee.com>
2023-12-21 23:51:32 +0700

    feat: add features on post page
    - render placeholder when image is loading
    - restrict view when not subscribed or have not logged in

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -4,14 +4,17 @@ import type {
   InferGetStaticPropsType,
 } from "next"
 import Image from "next/image"
-import { ImageIcon } from "@radix-ui/react-icons"
+import Link from "next/link"
 import Balancer from "react-wrap-balancer"
 
 import type { Post } from "@/types/api"
+import { getPlaceholder } from "@/lib/plaiceholder"
+import { useStore } from "@/lib/store"
 import { formatDate, readingTime, toSentenceCase } from "@/lib/utils"
 import { AspectRatio } from "@/components/ui/aspect-ratio"
 import { Badge } from "@/components/ui/badge"
-import { RootLayout } from "@/components/layouts/root"
+import { Button } from "@/components/ui/button"
+import { PostLayout } from "@/components/layouts/post"
 import { PostActions } from "@/components/post-actions"
 import { RecommendedPosts } from "@/components/recommended-posts"
 
@@ -31,9 +34,10 @@ export const getStaticPaths: GetStaticPaths = async () => {
   }
 }
 
-export const getStaticProps: GetStaticProps<{ post: Post }> = async (
-  context,
-) => {
+export const getStaticProps: GetStaticProps<{
+  post: Post
+  placeholder: string | null
+}> = async (context) => {
   const url = new URL(
     `/posts?slug=${context.params?.slug}`,
     process.env.NEXT_PUBLIC_DB_URL,
@@ -49,13 +53,19 @@ export const getStaticProps: GetStaticProps<{ post: Post }> = async (
   }
 
   return {
-    props: { post },
+    props: { post, placeholder: await getPlaceholder(post.image) },
   }
 }
 
 export default function PostPage({
   post,
+  placeholder,
 }: InferGetStaticPropsType<typeof getStaticProps>) {
+  const user = useStore((state) => state.user)
+
+  const content = post.content.split("\n")
+  const isRestricted = !user?.subscription.isSubscribed && post.isPremium
+
   return (
     <>
       <section className="mx-auto mb-10 flex max-w-screen-md lg:max-w-screen-lg">
@@ -65,19 +75,18 @@ export default function PostPage({
             alt={post.title}
             width={1600}
             height={1000}
+            placeholder={placeholder ? "blur" : "empty"}
+            blurDataURL={placeholder ?? undefined}
+            quality={90}
             className="absolute z-10 aspect-[16/9] object-cover"
           />
-          {/* TODO display only when image failed to load */}
-          <div className="absolute flex h-full w-full items-center justify-center bg-muted">
-            <ImageIcon className="h-20 w-20 text-muted-foreground" />
-          </div>
         </AspectRatio>
       </section>
 
       <section className="container mb-10 max-w-screen-md space-y-7 lg:max-w-screen-lg">
         {post.isPremium && (
           <div className="text-center">
-            <Badge className="bg-yellow-600 text-sm hover:bg-yellow-700">
+            <Badge className="bg-accent-premium text-sm hover:bg-accent-premium/80">
               Premium
             </Badge>
           </div>
@@ -106,11 +115,28 @@ export default function PostPage({
       </section>
 
       <section className="container mb-10 max-w-screen-md lg:max-w-[900px]">
-        {post.content.split("\n").map((paragraph, index) => (
-          <p key={index} className="mb-4 text-lg !leading-relaxed lg:text-xl">
+        {(isRestricted
+          ? content.slice(0, Math.floor(content.length / 2))
+          : content
+        ).map((paragraph) => (
+          <p
+            key={paragraph.slice(0, 20)}
+            className="mb-4 text-lg !leading-relaxed lg:text-xl"
+          >
             {paragraph}
           </p>
         ))}
+        {isRestricted && (
+          <div className="z-10 -translate-y-24 space-y-10 bg-gradient-to-t from-background from-60% to-transparent to-100% px-20 pb-5 pt-28 text-center">
+            <p className="text-lg font-semibold">
+              Don&apos;t miss out on premium contents written by the best
+              authors in the world, only for subscribed users.
+            </p>
+            <Button size="lg" asChild>
+              <Link href="/dashboard/billing">Subscribe Now</Link>
+            </Button>
+          </div>
+        )}
       </section>
 
       <section className="container mb-10 flex max-w-screen-md justify-center">
@@ -123,5 +149,5 @@ export default function PostPage({
 }
 
 PostPage.getLayout = function getLayout(page: React.ReactElement) {
-  return <RootLayout>{page}</RootLayout>
+  return <PostLayout>{page}</PostLayout>
 }
