6e308ad9b86f1598f9ee30377e7ca5ef092003e9
Tifan <tifan.avianto@shopee.com>
2023-12-24 22:05:14 +0700

    feat: add user revalidation on app mount

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,7 @@ import { Toaster } from "sonner"
 import { SWRConfig } from "swr"
 
 import type { AppPropsWithLayout } from "@/types/next"
-import { fetcher } from "@/lib/fetchers"
+import { fetcher, getUser } from "@/lib/fetchers"
 import { useStore } from "@/lib/store"
 import { BreakpointIndicator } from "@/components/breakpoint-indicator"
 
@@ -12,9 +12,35 @@ import "@/styles/globals.css"
 export default function App({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout ?? ((page) => page)
 
+  const user = useStore((state) => state.user)
+  const updateUser = useStore((state) => state.updateUser)
+
   React.useEffect(() => {
     useStore.persist.rehydrate()
   }, [])
+
+  React.useEffect(() => {
+    if (!user) return
+
+    const revalidateUser = async () => {
+      const { success, data } = await getUser({
+        email: user.email,
+        password: user.password,
+      })
+
+      if (
+        success &&
+        data &&
+        JSON.stringify(user.subscription) !== JSON.stringify(data.subscription)
+      ) {
+        updateUser(data)
+      }
+    }
+
+    revalidateUser()
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [user])
 
   return (
     <>
